e472c821f828c8cdbf7777c222033433bfab2afb
Ian Berry <iberry@raxnet.net>
2003-05-30 03:55:01 +0000

    added copyright header
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cactid_unix@1003 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- locks.c
+++ locks.c
@@ -1,3 +1,27 @@
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2003 Ian Berry                                            |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | cactid: a backend data gatherer for cacti                               |
+ +-------------------------------------------------------------------------+
+ | This poller would not have been possible without:                       |
+ |    - Rivo Nurges (rrd support, mysql poller cache, misc functions)      |
+ |    - RTG (core poller code, pthreads, snmp, autoconf examples)          |
+ +-------------------------------------------------------------------------+
+ | - raXnet - http://www.raxnet.net/                                       |
+ +-------------------------------------------------------------------------+
+*/
+
 #include <pthread.h>
 
 static pthread_mutex_t crew_lock = PTHREAD_MUTEX_INITIALIZER;
