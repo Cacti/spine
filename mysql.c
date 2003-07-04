9699987ae66b522854c6c045ecd01fa297cfd3c0
Ian Berry <iberry@raxnet.net>
2003-07-04 05:02:12 +0000

    formatting/naming cleanup
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cactid_unix@1076 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- mysql.c
+++ mysql.c
@@ -25,20 +25,23 @@
 #include "common.h"
 #include "cactid.h"
 
-
-int db_insert(char *query, MYSQL * mysql)
-{
-    if (set.verbose >= HIGH)
-	printf("SQL: %s\n", query);
-    if (mysql_query(mysql, query)) {
-	if (set.verbose >= LOW)
-	    fprintf(stderr, "** MySQL Error: %s\n", mysql_error(mysql));
-	return (FALSE);
-    } else
-	return (TRUE);
+int db_insert(char *query, MYSQL *mysql) {
+	if (set.verbose >= HIGH) {
+		printf("SQL: %s\n", query);
+	}
+	
+	if (mysql_query(mysql, query)) {
+		if (set.verbose >= LOW) {
+			fprintf(stderr, "** MySQL Error: %s\n", mysql_error(mysql));
+		}
+		
+		return (FALSE);
+	}else{
+		return (TRUE);
+	}
 }
 
-MYSQL_RES *db_query(MYSQL * mysql, char *query) {
+MYSQL_RES *db_query(MYSQL *mysql, char *query) {
 	MYSQL_RES *mysql_res;
 
 	mysql_query(mysql, query);
@@ -48,21 +51,22 @@ MYSQL_RES *db_query(MYSQL * mysql, char *query) {
 }
 
 
-int rtg_dbconnect(char *database, MYSQL * mysql)
-{
-    if (set.verbose >= LOW)
-	printf("Connecting to MySQL database '%s' on '%s'...", database, set.dbhost);
-    mysql_init(mysql);
-    if (!mysql_real_connect
-     (mysql, set.dbhost, set.dbuser, set.dbpass, database, 0, NULL, 0)) {
-	fprintf(stderr, "** Failed: %s\n", mysql_error(mysql));
-	return (-1);
-    } else
-	return (0);
+int db_connect(char *database, MYSQL *mysql) {
+	if (set.verbose >= LOW) {
+		printf("Connecting to MySQL database '%s' on '%s'...", database, set.dbhost);
+	}
+	
+	mysql_init(mysql);
+    	
+	if (!mysql_real_connect(mysql, set.dbhost, set.dbuser, set.dbpass, database, 0, NULL, 0)) {
+		fprintf(stderr, "** Failed: %s\n", mysql_error(mysql));
+		return (-1);
+	}else{
+		return (0);
+	}
 }
 
 
-void rtg_dbdisconnect(MYSQL * mysql)
-{
-    mysql_close(mysql);
+void db_disconnect(MYSQL *mysql) {
+	mysql_close(mysql);
 }
