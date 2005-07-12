5d6f02bf9b3151ed519ced366fbf213190ec55fd
Larry Adams <thewitness@cacti.net>
2005-07-12 21:43:39 +0000

    remove resource code for compatibility
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cactid_unix@3065 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- php.c
+++ php.c
@@ -34,7 +34,6 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/select.h>
-#include <sys/resource.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
@@ -149,27 +148,9 @@ int php_init() {
 	int  cancel_state;
 	char *result_string;
 
-	/* variable to set/get environment limitations */
-	struct rlimit ResourceLimits;
-    struct rusage Usage;
-
 	/* initialize the php process id */
 	set.php_sspid = 0;
 	
-    if (getrlimit(RLIMIT_STACK,&ResourceLimits) == 0) {
-		if (set.verbose == POLLER_VERBOSITY_DEBUG) {
-			printf("DEBUG: Current Max Memory Allocation is '%i' bytes\n", ResourceLimits.rlim_cur);
-	        printf("DEBUG: Maximum Max Memory Allocation is '%i' bytes\n", ResourceLimits.rlim_max);
-		}
-    }
-
-    if (getrlimit(RLIMIT_AS,&ResourceLimits) == 0) {
-		if (set.verbose == POLLER_VERBOSITY_DEBUG) {
-			printf("DEBUG: Current Address Space Allocation is '%i' bytes\n", ResourceLimits.rlim_cur);
-	        printf("DEBUG: Maximum Address Space Allocation is '%i' bytes\n", ResourceLimits.rlim_max);
-		}
-    }
-
 	if (set.verbose == POLLER_VERBOSITY_DEBUG) {
 		snprintf(logmessage, LOGSIZE-1, "DEBUG: PHP Script Server Routine Starting\n");
 		cacti_log(logmessage);
