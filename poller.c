87d0260761497a6f0eb18b6cf93cbdc35be56398
Larry Adams <thewitness@cacti.net>
2006-09-23 16:02:26 +0000

    correct 2 additional beta issues
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cactid@3785 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- poller.c
+++ poller.c
@@ -399,14 +399,14 @@ void poll_host(int host_id) {
 
 						snprintf(query3, BUFSIZE, "replace into poller_command (poller_id, time, action, command) values (0, NOW(), %i, '%i:%i')", POLLER_COMMAND_REINDEX, host->id, reindex->data_query_id);
 						db_insert(&mysql, query3);
-						assert_fail = FALSE;
+						assert_fail = TRUE;
 						previous_assert_failure = TRUE;
 					}else if ((!strcmp(reindex->op, "<")) && (strtoll(reindex->assert_value, (char **)NULL, 10) > strtoll(poll_result, (char **)NULL, 10))) {
 						CACTID_LOG_HIGH(("Host[%i] ASSERT: '%s' .lt. '%s' failed. Recaching host '%s', data query #%i\n", host->id, reindex->assert_value, poll_result, host->hostname, reindex->data_query_id));
 
 						snprintf(query3, BUFSIZE, "replace into poller_command (poller_id, time, action, command) values (0, NOW(), %i, '%i:%i')", POLLER_COMMAND_REINDEX, host->id, reindex->data_query_id);
 						db_insert(&mysql, query3);
-						assert_fail = FALSE;
+						assert_fail = TRUE;
 						previous_assert_failure = TRUE;
 					}
 
