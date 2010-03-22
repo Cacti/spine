efb8b56c89a564efb31763162e861223f8a0e7a9
Larry Adams <thewitness@cacti.net>
2010-03-22 01:45:47 +0000

    QA: Insure spine compiles on "old" platforms.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/spine/branches/0.8.7@5705 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- snmp.c
+++ snmp.c
@@ -152,7 +152,9 @@ void *snmp_host_init(int host_id, char *hostname, int snmp_version, char *snmp_c
 	#ifdef USE_NET_SNMP
 		/* Bind to snmp_clientaddr if specified */
 		if (NULL != set.snmp_clientaddr  && strlen(set.snmp_clientaddr) > 0) {
+			#if SNMP_LOCALNAME == 1
 			session.localname = strdup(set.snmp_clientaddr);
+			#endif
 		}
 
 		/* Prevent update of the snmpapp.conf file */
