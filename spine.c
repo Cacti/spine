37f455294763add00a5f17992ca720480c62c21b
Tony Roman <rony@cacti.net>
2011-09-26 00:19:22 +0000

    Remove extra + sign on logging message
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/spine/branches/0.8.7@6810 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- spine.c
+++ spine.c
@@ -559,7 +559,7 @@ int main(int argc, char *argv[]) {
 
 	/* mark the spine process as started */
 	if (!set.pre087g) {
-		SPINE_LOG_MEDIUM(("NOTE: Spine is behaving in a 0.8.7g++ manner"));
+		SPINE_LOG_MEDIUM(("NOTE: Spine is behaving in a 0.8.7g+ manner"));
 		snprintf(querybuf, BIG_BUFSIZE, "INSERT INTO poller_time (poller_id, pid, start_time, end_time) VALUES (%i, %i, NOW(), '0000-00-00 00:00:00')", set.poller_id, getpid());
 		db_insert(&mysql, querybuf);
 	}else{
