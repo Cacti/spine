861c5a658696bd98f258726231346fe90e68da5c
TheWitness <thewitness@cacti.net>
2026-03-11 22:44:31 -0400

    Make sure all file paths are at most BUFSIZE

--- spine.h
+++ spine.h
@@ -372,9 +372,9 @@ typedef struct config_struct {
 	char   db_user[BUFSIZE];
 	char   db_pass[BUFSIZE];
 	int    db_ssl;
-	char   db_ssl_key[BIG_BUFSIZE];
-	char   db_ssl_cert[BIG_BUFSIZE];
-	char   db_ssl_ca[BIG_BUFSIZE];
+	char   db_ssl_key[BUFSIZE];
+	char   db_ssl_cert[BUFSIZE];
+	char   db_ssl_ca[BUFSIZE];
 	int    d_b;
 	unsigned int db_port;
 	char   dbversion[BUFSIZE];
