7563618027e856340ba3c190e308d3a7ad565e51
Thomas Vincent <thomasvincent@gmail.com>
2026-04-17 03:29:45 -0700

    fix(php): break readpipe loop when buffer full
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- src/php.c
+++ src/php.c
@@ -273,6 +273,7 @@ char *php_readpipe(int php_process, char *command) {
 				if (bptr >= result_string+RESULTS_BUFFER) {
 					SPINE_LOG(("ERROR: SS[%i] The Script Server result was longer than the acceptable range", php_process));
 					SET_UNDEFINED(result_string);
+					break;
 				}
 		}
 
