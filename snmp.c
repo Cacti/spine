aad0f1e264e4f3ed9dbba493d7eca144d63f4bc6
Thomas Vincent <thomasvincent@gmail.com>
2026-09-05 23:32:33 -0700

    fix(snmp): show downgrades at default verbosity
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- snmp.c
+++ snmp.c
@@ -364,12 +364,12 @@ void *snmp_host_init(int host_id, char *hostname, int snmp_version, char *snmp_c
 
 		if (spine_snmpv3_protocol_is_set(snmp_auth_protocol) !=
 			spine_snmpv3_passphrase_is_set(snmp_password)) {
-			SPINE_LOG_MEDIUM(("SNMP: Device[%i] WARNING incomplete authentication settings; Cacti's effective security level is noAuthNoPriv.", host_id));
+			SPINE_LOG_LOW(("SNMP: Device[%i] WARNING incomplete authentication settings; Cacti's effective security level is noAuthNoPriv.", host_id));
 		}
 
 		if (spine_snmpv3_protocol_is_set(snmp_priv_protocol) !=
 			spine_snmpv3_passphrase_is_set(snmp_priv_passphrase)) {
-			SPINE_LOG_MEDIUM(("SNMP: Device[%i] WARNING incomplete privacy settings; Cacti's effective security level does not include encryption.", host_id));
+			SPINE_LOG_LOW(("SNMP: Device[%i] WARNING incomplete privacy settings; Cacti's effective security level does not include encryption.", host_id));
 		}
 
 		/* A protocol that is set but unrecognised is a configuration error at
